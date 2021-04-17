5eac88817f769f83f6a44f02dd3e8e756ff64ee3
Khairul Kabir <kabir@syntecglobal.net>
2021-04-17 14:25:08 +0600

    first commit

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = { 2,7,8,9,13,19,21};
+            //binarySearch(numArray, 7);
+            Console.WriteLine("This is your arry: ");
+            foreach (var item in numArray)
+            {
+                Console.Write(item+",");
+            }
+            Console.WriteLine("\n"+binarySearch(numArray, 7));
+            Console.WriteLine(binarySearch(numArray, 13));
+        }
+        public static dynamic binarySearch(int[] numArray, int target)
+        {
+            int left = 0;
+            int right = numArray.Length - 1;
+            while (left < right)
+            {
+                int mid = Convert.ToInt32((left+right)/2);
+                if (target == numArray[mid])
+                {
+                    return mid;
+                }else if (target<numArray[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return false;
+        }
+    }
+}
